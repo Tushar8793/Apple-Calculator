121ceebd0c4d312bb2aedebf624e36f3b8f72576
Tushar8793 <sharmatushar8793@gmail.com>
2024-05-23 22:45:20 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let buttons = document.querySelectorAll("button");
+let input = document.querySelector("input");
+
+for (let button of buttons) {
+    button.addEventListener("click", function (e) {
+        let btnText = e.target.innerText;
+        if (btnText === "AC") {
+            input.value = "";
+        }
+        else if (btnText === "=") {
+            try {
+                input.value = eval(input.value);
+            }
+            catch (err) {
+                input.value = "Gian H Aap"; 
+            }
+        }
+        else
+            input.value += btnText;
+    });
+}   
